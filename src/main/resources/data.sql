106b7e21568c655965332d74e80cebbd0c824e20
gabrielpfvr <gabriel_1337@live.com>
2025-05-26 21:05:42 -0300

    remove initial sql

--- src/main/resources/data.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO users(id, name, email, password, role, created_at, birth_date)
-VALUES(100, 'admin', 'admin@test.com', '$2a$10$o2mJHEunGB6wDyE0a5MLC.IjrIHcv.r/RJH0tab0V6OlX4e55Tfkq', 'ADMIN', CURRENT_TIMESTAMP, CURRENT_DATE);
